481ac714269456161b349e70d318b65174392161
AlexYov <xxxx81971@yandex.ru>
2021-02-22 18:25:50 +0500

    ...

--- capture_process_video/cap_video.py
+++ capture_process_video/cap_video.py
@@ -1,7 +1,7 @@
 import cv2
 
 capture_video = cv2.VideoCapture(0)
-size_window = 0.85
+size_window = 0.8
 while True:
     _, frames = capture_video.read()
     new_frames = cv2.resize(frames, None, fx = size_window, fy = size_window, interpolation = cv2.INTER_AREA)
